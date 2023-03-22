b6c2975cdcb7e1b2952e0ce4fefbf6dcfa7d57bf
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-03-22 08:29:14 -0300

    Aula sobre variáveis e tipos

---
+++ aula 2/variaveis.js
@@ -0,0 +1,20 @@
+// let num1 = 5
+// const num2 = 3
+// var msg= "Olá, amigos! Esse é o número:"
+
+// console.log(msg, num1)
+// console.log(num1 + num2)
+
+// console.log(typeof num1)
+// console.log(typeof msg)
+
+//JSON - JavaScript Object Notation
+
+let usuario = { 
+    name: "Diego",
+    idade: 17,
+    calvo: false,
+}
+
+console.log(usuario)
+
